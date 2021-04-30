70de2549b6ae791b3e4e6355b230030f72e50c51
JKRUS <jkrus1591@gmail.com>
2021-04-30 17:43:40 +0300

    First Commit

---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,18 @@
+package apiserver
+
+//API Server
+type APIServer struct {
+	config *Config
+}
+
+//New ...
+func New(config *Config) *APIServer {
+	return &APIServer{
+		config: config,
+	}
+}
+
+func (s *APIServer) Start() error {
+
+	return nil
+}
